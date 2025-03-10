1b20f718ab91f5331c83be6b7715a45327a0e5ac
donut_1906 <cuongbc.hdpc@gmail.com>
2025-03-11 01:28:52 +0700

    Bai tap 1: Hien thi loi chao

---
+++ VariableDataTYpeOperator/ShowSayHello/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ShowSayHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string inputName = Console.ReadLine();
+            Console.WriteLine($"Hello: {inputName}");
+        }
+    }
+}
