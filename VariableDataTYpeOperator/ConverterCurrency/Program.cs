f7f2e1108365dc6dca53b8f268cf27c58b180cfa
donut_1906 <cuongbc.hdpc@gmail.com>
2025-03-11 01:53:00 +0700

    Bai tapj 2: Chuong  trinh doi tien te

---
+++ VariableDataTYpeOperator/ConverterCurrency/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace ConverterCurrency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rateCurency = 23_000; //Tỷ giá
+            Console.Write("Enter USD: ");
+            float yourMoneyInUSD = float.Parse(Console.ReadLine()); //Nhập vào số tiền USD muốn quy đổi 
+            CultureInfo culture = new CultureInfo("vi-VN"); //Hiển thị tiền tệ theo đồng Việt Nam
+            Console.WriteLine($"Your Monney in VND is: {(yourMoneyInUSD * rateCurency).ToString("C", culture)}");
+
+        }
+    }
+}
